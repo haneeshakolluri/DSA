9cc6c8811fcc5adb3f1a1037d0114e5eedcf5edf
Haneesha Kolluri <151764166+haneeshakolluri@users.noreply.github.com>
2026-01-13 22:51:55 +0530

    Time: 8 ms (31.15%) | Memory: 20.4 MB (6.24%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            d[i]=d.get(i,0)+1
+        for k,v in d.items():
+            if v!=1:
+                return k
+        
